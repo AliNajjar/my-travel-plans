e380fa5fce040829800d2db347ae7e10b8da54df
Ali Najjar <najar3615@gmail.com>
2018-03-05 22:27:53 +0200

    Creat class ATM

--- atm.py
+++ atm.py
@@ -1,11 +1,19 @@
-def withdraw(balance, request):
-    
-    print "current balance =", balance
+class ATM:
+    def __init__(self, balance, bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
+
+def withdraw(self, request):
+    print "welcome to", self.bank_name
+    print "current balance =", self.balance
+    print "========================="
+
     cash = [100, 50, 10, 5, 4, 3, 2, 1]
-    if request > balance:
+    if request > self.balance:
         print "there is no money sufficient"
-    elif request <= balance:
-        balance -= request
+        print "========================="
+    elif request <= self.balance:
+        self.balance -= request
         for i in cash:
             while request >= i:
                 print "give:", i
@@ -14,11 +22,19 @@ def withdraw(balance, request):
 
     else:
         print "Request should be more than zero"
+        print "========================="
 
-    return balance
+    return self.balance
 
-balance = 500
 
-balance = withdraw(balance, 277)
-balance = withdraw(balance, 30)
-balance = withdraw(balance, 5)
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm2.withdraw(2000)
