5cf7657553ca7298c6ee1509017bd7081223e2d9
Ali Najjar <najar3615@gmail.com>
2018-03-06 02:36:36 +0200

    Get an invoice

--- atm.py
+++ atm.py
@@ -2,6 +2,7 @@ class ATM:
     def __init__(self, balance, bank_name):
         self.balance = balance
         self.bank_name = bank_name
+        self.withdrawals_ls = []
 
     def withdraw(self, request):
         print "welcome to", self.bank_name
@@ -13,6 +14,7 @@ class ATM:
             print "there is no money sufficient"
             print "========================="
         elif request <= self.balance:
+            self.withdrawals_ls.append(request)
             self.balance -= request
             for i in cash:
                 while request >= i:
@@ -25,6 +27,11 @@ class ATM:
             print "========================="
 
         return self.balance
+    
+    def show_withdrawals(self):
+        for withdrawal in self.withdrawals_ls:
+            print "withdrawals:", withdrawal
+        print "========================="
 
 
 balance1 = 500
@@ -34,7 +41,9 @@ atm1 = ATM(balance1, "Smart Bank")
 atm2 = ATM(balance2, "Baraka Bank")
 
 atm1.withdraw(277)
-atm1.withdraw(800)
+atm1.withdraw(80)
+atm1.show_withdrawals()
 
 atm2.withdraw(100)
-atm2.withdraw(2000)
+atm2.withdraw(200)
+atm2.show_withdrawals()
