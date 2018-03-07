02f192be31aea5aeb58ed940f946ec44080c9ea0
Ali Najjar <najar3615@gmail.com>
2018-03-07 06:44:40 +0200

    improved if condition

--- atm.py
+++ atm.py
@@ -22,14 +22,15 @@ class ATM:
         if request > self.balance:
             print "There is no money sufficient"
             print "========================="
-        elif request <= self.balance:
+
+        elif request <= 0:
+            print "Request should be more than zero"
+            print "========================="
+
+        else:
             self.withdrawals_ls.append(request)
             self.balance -= request
             self.process_request(request)
-
-        else:
-            print "Request should be more than zero"
-            print "========================="
 
         return self.balance
 
@@ -47,8 +48,10 @@ atm2 = ATM(balance2, "Baraka Bank")
 
 atm1.withdraw(277)
 atm1.withdraw(80)
+atm1.withdraw(0)
 atm1.show_withdrawals()
 
 atm2.withdraw(100)
 atm2.withdraw(200)
+atm2.withdraw(-75)
 atm2.show_withdrawals()
